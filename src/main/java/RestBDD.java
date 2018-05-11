65a7b6633aec26060861ababd061e63e093b4d82
User1 <varshachhillar@gmail.com>
2018-05-11 14:48:36 +0530

    rest

---
+++ src/main/java/RestBDD.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+
+
+public class RestBDD {
+
+    @Test
+    public void testnumbercir2017() {
+    	given()
+    	
+    }
+
+}
